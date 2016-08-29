92df9b71973084b92a21f8e81af2514e7cc6c785
Kyle <kcd5059@gmail.com>
2016-08-29 14:54:52 -0400

    Edited after Greg's suggestions

--- src/ssa/Student.java
+++ src/ssa/Student.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class Student {
 
@@ -21,16 +23,11 @@ public class Student {
 		this.lastName = lastName;
 		this.eyeColor = eyeColor;
 		this.monthsEmployed = monthsEmployed;
-		classRoster.add(this);
 		database.put(this.id, this);
 	}
 
 	public Student() {
-
-	}
-
-	public void addToRoster(Student student) {
-		classRoster.add(student);
+		setup();
 	}
 	
 	private void printStudent(Student student) {
@@ -39,33 +36,37 @@ public class Student {
 				+ String.format("%-16s", student.lastName) + "   " + String.format("%-9s", student.eyeColor) + "   "
 				+ String.format("%8s", student.monthsEmployed));
 	}
+	
+	private void setup() {
+		
+		classRoster.add(new Student("001122", "Michael", "Clair", "Other", 12));
+		classRoster.add(new Student("001112", "Stephen", "Rook", "Brown", 11));
+		classRoster.add(new Student("005295", "Kyle", "Deen", "Blue", 2));
+		classRoster.add(new Student("004400", "Kevin", "Tran", "Other", 12));
+		classRoster.add(new Student("474143", "Jonathan", "Stafford", "Brown", 13));
+		
+		classRoster.add(new Student("132617", "Reuben", "Turner", "Blue", 12));
+		classRoster.add(new Student("306700", "Li", "Lui", "Brown", 12));
+		classRoster.add(new Student("215296", "Joshua", "Franey", "Other", 27));
+		classRoster.add(new Student("523420", "Gabriel", "Hamilton", "Other", 10));
+		classRoster.add(new Student("001014", "Aisha", "Covington", "Brown", 10));
+		
+		classRoster.add(new Student("006789", "Arun", "Soma", "Brown", 2));
+		classRoster.add(new Student("009999", "Steve", "Ellwood", "Other", 2));
+		classRoster.add(new Student("343769", "Shaquil", "Timmons", "Brown", 11));
+		classRoster.add(new Student("001449", "Karen", "Reiter", "Blue", 10));
+		classRoster.add(new Student("267252", "Dax", "Richards", "Brown", 12));
+		
+		classRoster.add(new Student("229949", "Mike", "Sykes", "Brown", 13));
+		classRoster.add(new Student("772223", "Daniel", "Kiros", "Brown", 3));
+		classRoster.add(new Student("004444", "Peter", "Choi", "Brown", 2));
+		classRoster.add(new Student("005255", "Joe", "Hill", "Brown", 13));
+		classRoster.add(new Student ("008888", "Evan", "Tizard", "Brown", 12));
+
+	}
 
 	public void printClassRoster() {
 		
-		Student s1 = new Student("001122", "Michael", "Clair", "Other", 12);
-		Student s2 = new Student("001112", "Stephen", "Rook", "Brown", 11);
-		Student s3 = new Student("005295", "Kyle", "Deen", "Blue", 2);
-		Student s4 = new Student("004400", "Kevin", "Tran", "Other", 12);
-		Student s5 = new Student("474143", "Jonathan", "Stafford", "Brown", 13);
-		
-		Student s6 = new Student("132617", "Reuben", "Turner", "Blue", 12);
-		Student s7 = new Student("306700", "Li", "Lui", "Brown", 12);
-		Student s8 = new Student("215296", "Joshua", "Franey", "Other", 27);
-		Student s9 = new Student("523420", "Gabriel", "Hamilton", "Other", 10);
-		Student s10 = new Student("001014", "Aisha", "Covington", "Brown", 10);
-		
-		Student s11 = new Student("006789", "Arun", "Soma", "Brown", 2);
-		Student s12 = new Student("009999", "Steve", "Ellwood", "Other", 2);
-		Student s13 = new Student("343769", "Shaquil", "Timmons", "Brown", 11);
-		Student s14 = new Student("001449", "Karen", "Reiter", "Blue", 10);
-		Student s15 = new Student("267252", "Dax", "Richards", "Brown", 12);
-		
-		Student s16 = new Student("229949", "Mike", "Sykes", "Brown", 13);
-		Student s17 = new Student("772223", "Daniel", "Kiros", "Brown", 3);
-		Student s18 = new Student("004444", "Peter", "Choi", "Brown", 2);
-		Student s19 = new Student("005255", "Joe", "Hill", "Brown", 13);
-		Student s20 = new Student ("008888", "Evan", "Tizard", "Brown", 12);
-
 		System.out.println("----------PART 2----------");
 		System.out.println("Student class roster");
 		System.out.println();
@@ -91,20 +92,29 @@ public class Student {
 		System.out.println("Empl Id   First name         Last name          Eye color   Months at SSA");
 		System.out.println("=======   ================   ================   =========   =============");
 		
-		Collections.sort(classRoster, new Comparator<Student>() {
-			@Override
-			public int compare(Student student1, Student student2) {
-				return student1.id.compareTo(student2.id);
-			}
-		});
+//		Collections.sort(classRoster, new Comparator<Student>() {
+//			@Override
+//			public int compare(Student student1, Student student2) {
+//				return student1.id.compareTo(student2.id);
+//			}
+//		});
 		
-		for (int student = 0; student < classRoster.size(); student++) {
+		Set<String> idSet = database.keySet();
+		ArrayList<String> idList = new ArrayList<>();
+		
+		for (String id : idSet) {
+			idList.add(id);
+		}
+		
+		Collections.sort(idList);
+		
+		for (int id = 0; id < idList.size(); id++) {
 
-			if ( classRoster.get(student).firstName.equals("Kyle") ) {
+			if ( idList.get(id).equals("005295") ) {
 				
-				Student studentBefore = database.get(classRoster.get(student - 1).id);
-				Student me = database.get(classRoster.get(student).id);
-				Student studentAfter = database.get(classRoster.get(student + 1).id);
+				Student studentBefore = database.get(idList.get(id - 1));
+				Student me = database.get("005295");
+				Student studentAfter = database.get(idList.get(id + 1));
 				
 				printStudent(studentBefore);
 				printStudent(me);
